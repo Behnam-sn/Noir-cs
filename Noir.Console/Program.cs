25b238af9c6364b6a9b55f4b5ebbfa00abac7f1e
Behnam Salehi <behnam.dev@outlook.com>
2024-05-03 22:35:55 +0330

    Implement Console Program Logic

--- Noir.Console/Program.cs
+++ Noir.Console/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Noir.Console
+﻿namespace Noir.Console;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        while (true)
         {
-            System.Console.WriteLine("Hello, World!");
+            System.Console.Write("Enter The Command: ");
+            var command = System.Console.ReadLine()?.ToLower();
+
+            if (command == "movie" || command == "m")
+            {
+                HandleMovie();
+            }
+
+            if (command == "series" || command == "s")
+            {
+                HandleSeries();
+            }
+
+            if (command == "help" || command == "h")
+            {
+                PrintCommands();
+            }
+
+            if (command == "exit" || command == "e")
+            {
+                break;
+            }
         }
+    }
+
+    private static void HandleMovie()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void HandleSeries()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void PrintCommands()
+    {
+        System.Console.WriteLine("movie, m");
+        System.Console.WriteLine("series, s");
+        System.Console.WriteLine("help, h");
+        System.Console.WriteLine("exit, e");
     }
 }
