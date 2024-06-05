6371ffcf31edc35d1e0694103df1e2b673cc5d2e
Behnam Salehi <behnam.dev@outlook.com>
2024-06-06 03:24:36 +0330

    Create SeriesRenameProcessor.cs

---
+++ Noir.Domain/SeriesRenameProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Noir.Domain.Contracts;
+
+namespace Noir.Domain;
+
+public class SeriesRenameProcessor : IProcessor
+{
+    public RenameContext? Preview(string fileNameWithPath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public RenameContext? Rename(string fileNameWithPath)
+    {
+        throw new NotImplementedException();
+    }
+}
