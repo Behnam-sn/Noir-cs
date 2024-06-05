6f3abd5808d314491bea5084eaf8e9e86321e7d0
Behnam Salehi <behnam.dev@outlook.com>
2024-06-06 03:21:09 +0330

    Create MovieTests.cs

---
+++ Noir.Domain.Tests/MovieTests.cs
@@ -0,0 +1,18 @@
+namespace Noir.Domain.Tests;
+
+public class MovieTests
+{
+    [Theory]
+    [InlineData("", null)]
+    [InlineData("Babylon.2022.720p.10bit.WEBRip.6CH.x265.HEVC-PSA.mkv", "Babylon.mkv")]
+    [InlineData("Jimmy.Carr.Natural.Born.Killer.2024.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Jimmy Carr Natural Born Killer.mkv")]
+    public void PareseTest(string fileName, string? expected)
+    {
+        // Given
+        // When
+        var movie = Movie.Parse(fileName: fileName);
+        var actual = movie?.ToString();
+        // Then
+        Assert.Equal(expected, actual);
+    }
+}
