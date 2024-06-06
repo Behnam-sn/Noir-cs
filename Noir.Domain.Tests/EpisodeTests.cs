f80d2ba51e7a5aa5d64358a1f292f678be0631ed
Behnam Salehi <behnam.dev@outlook.com>
2024-06-06 03:34:59 +0330

    Rename ProcessTest to ParseTest

--- Noir.Domain.Tests/EpisodeTests.cs
+++ Noir.Domain.Tests/EpisodeTests.cs
@@ -7,7 +7,7 @@ public class EpisodeTests
     [InlineData("", null)]
     [InlineData("Regular.Show.S01E01.720p.BluRay.x265", "Regular Show S01E01")]
     [InlineData("Regular.Show.S08E27.A.Regular.Epic.Final.Battle.Part.1.720p.STAN.WEB-DL.x264", "Regular Show S08E27")]
-    public void ProcessTest(string fileName, string? expected)
+    public void ParseTest(string fileName, string? expected)
     {
         // Given
         // When
