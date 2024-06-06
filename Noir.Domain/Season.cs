a901c40bde1c98596cc0ba2e6050eec6b67030a1
Behnam Salehi <behnam.dev@outlook.com>
2024-06-06 18:55:30 +0330

    Implement Season Parse method

--- Noir.Domain/Season.cs
+++ Noir.Domain/Season.cs
@@ -1,13 +1,15 @@
-﻿namespace Noir.Domain;
+﻿using System.Text.RegularExpressions;
+
+namespace Noir.Domain;
 
 public class Season
 {
-    public Season(int number)
+    public Season(string number)
     {
         Number = number;
     }
 
-    public int Number { get; }
+    public string Number { get; }
 
     public override string ToString()
     {
@@ -16,6 +18,18 @@ public class Season
 
     public static Season? Parse(string text)
     {
-        return null;
+        var textToLower = text.ToLower();
+        var seasonRegex = new Regex(@"s\d\de\d\d");
+        var matched = seasonRegex.Matches(textToLower).Select(m => m.Value).ToList();
+
+        var selected = matched.LastOrDefault();
+        var season = selected?.Substring(1, 2);
+
+        if (season is null)
+        {
+            return null;
+        }
+
+        return new Season(season);
     }
 }
