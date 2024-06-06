1a83fc00cd3243d39edf8b86ef15554006e61f95
Behnam Salehi <behnam.dev@outlook.com>
2024-06-06 18:56:12 +0330

    Implement Episode Parse Method

--- Noir.Domain/EpisodeIndex.cs
+++ Noir.Domain/EpisodeIndex.cs
@@ -1,16 +1,35 @@
-﻿namespace Noir.Domain;
+﻿using System.Text.RegularExpressions;
+
+namespace Noir.Domain;
 
 public class EpisodeIndex
 {
-    public int Number { get; }
+    public EpisodeIndex(string number)
+    {
+        Number = number;
+    }
+
+    public string Number { get; }
 
     public override string ToString()
     {
         return $"E{Number}";
     }
 
-    public static EpisodeIndex? Parse(string fileName)
+    public static EpisodeIndex? Parse(string text)
     {
-        throw new NotImplementedException();
+        var textToLower = text.ToLower();
+        var episodeRegex = new Regex(@"s\d\de\d\d");
+        var matched = episodeRegex.Matches(textToLower).Select(m => m.Value).ToList();
+
+        var selected = matched.LastOrDefault();
+        var episode = selected?.Substring(4, 2);
+
+        if (episode is null)
+        {
+            return null;
+        }
+
+        return new EpisodeIndex(episode);
     }
 }
