fe4573a610851033f874aa11cde1c1b9668f9124
Behnam Salehi <behnam.dev@outlook.com>
2024-07-02 04:30:33 +0330

    Update EpisodeRenameService.cs

--- Noir.Application/EpisodeRenameService.cs
+++ Noir.Application/EpisodeRenameService.cs
@@ -7,12 +7,6 @@ public class EpisodeRenameService : RenameServiceBase
     protected override string? GenerateNewFileName(string fileName)
     {
         var episode = Episode.Parse(fileName: fileName);
-
-        if (episode is null)
-        {
-            return null;
-        }
-
-        return episode.ToString();
+        return episode?.ToString();
     }
 }
