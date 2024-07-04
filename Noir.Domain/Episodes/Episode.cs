80039aaf6ebac85cfd91b81e68a79fbeba93dc1c
Behnam Salehi <behnam.dev@outlook.com>
2024-07-04 05:23:09 +0330

    Add Year property to Episode

--- Noir.Domain/Episodes/Episode.cs
+++ Noir.Domain/Episodes/Episode.cs
@@ -4,16 +4,18 @@ namespace Noir.Domain.Episodes;
 
 public class Episode
 {
-    public Episode(EpisodeTitle title, EpisodeIndex index, Quality? quality = null, FileExtension? extension = null)
+    public Episode(EpisodeTitle title, EpisodeIndex index, Year? year = null, Quality? quality = null, FileExtension? extension = null)
     {
         Title = title;
         Index = index;
+        Year = year;
         Quality = quality;
         Extension = extension;
     }
 
     public EpisodeTitle Title { get; }
     public EpisodeIndex Index { get; }
+    public Year? Year { get; }
     public Quality? Quality { get; }
     public FileExtension? Extension { get; }
 
@@ -29,17 +31,18 @@ public class Episode
             return null;
         }
 
-        var episodeIndex = EpisodeIndex.Parse(fileName);
+        var index = EpisodeIndex.Parse(fileName);
 
-        if (episodeIndex is null)
+        if (index is null)
         {
             return null;
         }
 
+        var year = Year.Parse(fileName);
         var quality = Quality.Parse(fileName);
-        var title = EpisodeTitle.Parse(fileName, episodeIndex, quality);
+        var title = EpisodeTitle.Parse(fileName, index, year, quality);
         var extension = FileExtension.Parse(fileName);
 
-        return new Episode(title, episodeIndex, quality, extension);
+        return new Episode(title, index, year, quality, extension);
     }
 }
