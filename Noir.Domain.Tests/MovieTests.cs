05aeaba76044b17963af658f7d4525df98fd0d70
Behnam Salehi <behnam.dev@outlook.com>
2024-07-04 08:04:48 +0330

    Update MovieTests.cs

--- Noir.Domain.Tests/MovieTests.cs
+++ Noir.Domain.Tests/MovieTests.cs
@@ -10,7 +10,6 @@ public class MovieTests
     [InlineData("Jimmy.Carr.Natural.Born.Killer.2024.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Jimmy Carr Natural Born Killer.mkv")]
     [InlineData("Invincible.Presenting.Atom.Eve.Special.Episode.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Invincible Presenting Atom Eve Special Episode.mkv")]
     [InlineData("Fantastic.Mr.Fox.2009.720p.10bit.6CH.x265.PSA.mkv", "Fantastic Mr. Fox.mkv")]
-    [InlineData("ABornxTale-1993-720p.mkv", "A Bornx Tale.mkv")]
     [InlineData("Race to Witch Mountain (2009) BluRay 720p.mkv", "Race to Witch Mountain.mkv")]
     public void ParseTest(string fileName, string? expected)
     {
