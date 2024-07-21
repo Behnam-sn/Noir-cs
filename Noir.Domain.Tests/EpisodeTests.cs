c8f153bfbbfd0425141e35397aac689b8b8fdc54
Behnam <behnam.dev@outlook.com>
2024-07-21 16:31:46 +0330

    Add a test case to EpisodeTests

--- Noir.Domain.Tests/EpisodeTests.cs
+++ Noir.Domain.Tests/EpisodeTests.cs
@@ -12,6 +12,7 @@ public class EpisodeTests
     [InlineData("Invincible.2021.S02E01.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Invincible S02E01.mkv")]
     [InlineData("Kaiju.No.8.S01E01.DUAL-AUDIO.JAP-ENG.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Kaiju No. 8 S01E01.mkv")]
     [InlineData("Solo.Leveling.A.K.A.Ore.dake.Level.Up.na.Ken.S01E01.JAPANESE.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Solo Leveling A.K.A. Ore dake Level Up na Ken S01E01.mkv")]
+    [InlineData("Fantastic.Mr.Fox.2009.720p.10bit.6CH.x265.PSA.mkv", "Fantastic Mr. Fox.mkv")]
     public void ParseTest(string fileName, string? expected)
     {
         // Given
