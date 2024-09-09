2d1b01c529dfdcbfa5956bb2034b4e796d9678d8
Behnam <behnam.dev@outlook.com>
2024-09-10 01:00:56 +0330

    Update MovieTests.cs

--- Noir.Domain.Tests/MovieTests.cs
+++ Noir.Domain.Tests/MovieTests.cs
@@ -11,6 +11,7 @@ public class MovieTests
     [InlineData("Invincible.Presenting.Atom.Eve.Special.Episode.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv", "Invincible Presenting Atom Eve Special Episode.mkv")]
     [InlineData("Fantastic.Mr.Fox.2009.720p.10bit.6CH.x265.PSA.mkv", "Fantastic Mr. Fox.mkv")]
     [InlineData("Race to Witch Mountain (2009) BluRay 720p.mkv", "Race to Witch Mountain.mkv")]
+    [InlineData("My.Neighbor.Totoro.A.K.A.Tonari.no.Totoro.1988.DUAL-AUDIO.JAP-ENG.720p.10bit.BluRay.2CH.x265.HEVC-PSA.mkv", "My Neighbor Totoro A.K.A. Tonari no Totoro.mkv")]
     public void ParseTest(string fileName, string? expected)
     {
         // Given
